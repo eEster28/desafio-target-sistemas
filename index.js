0bd60d131587b2f45ed03e47eb5a60900abf9058
Ester Moura <135028149+eEster28@users.noreply.github.com>
2024-09-26 07:44:51 -0300

    feat: adicionado segunda pergunta do teste

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//1) Soma de valores de 1 até INDICE
+// 1) Soma de valores de 1 até INDICE
 
 let INDICE = 13;
 let SOMA = 0;
@@ -7,6 +7,28 @@ for (let K = 1; K <= INDICE; K++) {
     SOMA += K;
 }
 
-console.log(SOMA); //A somo será 91
+console.log(SOMA); //A soma será 91
 
 
+// 2) Verificação de número na sequência de Fibonacci
+
+function isFibonacci(num) {
+    let a = 0, b = 1, next;
+
+    if (num === a || num === b) return true;
+
+    for (next = a + b; next <= num; next = a + b) {
+        if (next === num) return true;
+        a = b;
+        b = next;
+    }
+
+    return false;
+}
+
+const numero = 88; 
+if (isFibonacci(numero)) {
+    console.log(`${numero} pertence à sequência de Fibonacci.`);
+} else {
+    console.log(`${numero} não pertence à sequência de Fibonacci.`);
+}
