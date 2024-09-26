f172c36303c89df52828e47464b8cfd74a36fc54
Ester Moura <135028149+eEster28@users.noreply.github.com>
2024-09-26 08:04:48 -0300

    feat: adicionado quarta pergunta do teste

--- index.js
+++ index.js
@@ -70,3 +70,24 @@ function calcularFaturamento(faturamentoMensal) {
     console.log(`Dias acima da média: ${diasAcimaDaMedia}`);
 }
 
+
+//4) Percentual de faturamento por estado:
+
+const faturamentoPorEstado = {
+    "SP": 67836.43,
+    "RJ": 36678.66,
+    "MG": 29229.88,
+    "ES": 27165.48,
+    "Outros": 19849.53
+};
+
+function calcularPercentual(faturamentoPorEstado) {
+    const total = Object.values(faturamentoPorEstado).reduce((acc, curr) => acc + curr, 0);
+
+    Object.entries(faturamentoPorEstado).forEach(([estado, valor]) => {
+        const percentual = (valor / total) * 100;
+        console.log(`${estado}: ${percentual.toFixed(2)}%`);
+    });
+}
+
+calcularPercentual(faturamentoPorEstado);
